0b7bf2e7d798b8bc9fde1ba4fda6cb85ce1fb494
LasagnaCake <sponge.cake@tuta.io>
2024-12-22 01:12:28 -0300

    Update yield.hpp

--- src/new/ctl/ctl/async/co/yield.hpp
+++ src/new/ctl/ctl/async/co/yield.hpp
@@ -9,12 +9,21 @@ CTL_NAMESPACE_BEGIN
 
 /// @brief Cooperative routine facilities.
 namespace Co {
-	/// @brief Generates a coroutine that stalls for a given count of yields.
+	/// @brief Generates a coroutine that stalls a given amount of times.
 	/// @param count Yield count.
 	/// @return Awaitable promise.
+	/// @details
+	///		Generates a coroutine that will return `true` for the given amount of times passed.
+	///		Coroutine is always starts suspended.
+	///
+	///		Meant to be used in conjunction with a `while` loop — see example.
+	/// @example
+	///		auto yielder = yield(/*count*/);
+	///		while (yielder.next())
+	///			co_yield /*return type*/;
 	Promise<bool, false> yield(usize count) {
 		if (!count) co_return false;
-		while (--count)
+		while (count--)
 			co_yield true;
 		co_yield false;
 	}
