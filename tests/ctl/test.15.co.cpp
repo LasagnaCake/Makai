9e20bf1301eb8aa40f4d34bffb6840251e00643c
LasagnaCake <sponge.cake@tuta.io>
2024-12-22 20:53:54 -0300

    Update test.15.co.cpp

--- tests/ctl/test.15.co.cpp
+++ tests/ctl/test.15.co.cpp
@@ -14,10 +14,6 @@ CTL::Co::Routine cofun2(int ci) {
 		usize const time = rng.integer<usize>(0, 10);
 		DEBUGLN("Coroutine: ", ci, ", Cycle: ", i+1, ", Wait: ", time);
 		co_await CTL::Co::yield(time);
-		/*
-		auto yielder = CTL::Co::yield(rng.integer<usize>(1, 10));
-		while (yielder.next()) co_yield false;
-		*/
 	}
 	DEBUGLN("Coroutine: ", ci, ", Done!!!");
 	co_return;
