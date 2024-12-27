4db5bee78259bac1be4d02eb147828d3b0abd267
LasagnaCake <sponge.cake@tuta.io>
2024-12-26 21:35:38 -0300

    Update dialog.hpp

--- src/makai-ex/game/dialog.hpp
+++ src/makai-ex/game/dialog.hpp
@@ -4,6 +4,11 @@
 #include <makai/makai.hpp>
 
 namespace Makai::Ex::Game::Dialog {
+	struct IVisible {
+		virtual void show() = 0;
+		virtual void hide() = 0;
+	};
+
 	using Graphic = Handle<Graph::IGraphic>;
 
 	struct Content {
@@ -18,20 +23,34 @@ namespace Makai::Ex::Game::Dialog {
 		bool	autoplay	= false;
 	};
 
-	struct SpeechBubble {
+	struct Action {
+		String name;
+	};
+
+	struct SpeechBubble: IVisible {
 		Graph::Label	title;
 		Graph::Label	text;
 		Graphic			body;
 
-		void display(Content const& title, Content const& text);
+		virtual ~SpeechBubble() {}
+		void show() final;
+		void hide() final;
+		virtual void display(Content const& title, Content const& text);
 	};
 
-	struct Actor {
-		Graphic			body;
-		SpeechBubble	bubble;
-		CTL::Co::AlwaysSuspend enter();
-		CTL::Co::AlwaysSuspend leave();
-		CTL::Co::AlwaysSuspend say(Message const& what);
+	struct Actor: IVisible {
+		using Step = CTL::Co::AlwaysSuspend;
+		Graphic					body;
+		Handle<SpeechBubble>	bubble;
+
+		virtual ~Actor() {}
+		
+		virtual Step enter();
+		virtual Step leave();
+		void show() final;
+		void hide() final;
+		Step say(Message const& what);
+		Step act(Action const& what);
 	};
 
 	struct Scene {
