151dc1980d15d39bf755502b08c5e471630c57f6
LasagnaCake <sponge.cake@tuta.io>
2025-01-05 00:10:58 -0300

    Update compiler.hpp

--- src/makai-ex/game/dialog/dvm/compiler.hpp
+++ src/makai-ex/game/dialog/dvm/compiler.hpp
@@ -7,6 +7,181 @@
 
 /// @brief Dialog Virtual Machine.
 namespace Makai::Ex::Game::Dialog::DVM {
+	namespace RegexMatches {
+		constexpr String ANY_CHAR		= String("[\\S\\s]");
+		constexpr String PARAM_CHAR		= String("[^,]");
+		constexpr String NAME_CHAR		= String("[0-z\\-_]");
+		constexpr String NON_NAME_CHAR	= String("[^0-z\\-_]");
+		constexpr String COMPLEX_TOKEN	= String("[\\w&!@#$%&><+\\-_']");
+		constexpr String SIMPLE_TOKEN	= String("[*.,;]");
+
+		constexpr String makePack(String const& begin, String const& end) {
+			return String() + begin + ANY_CHAR + "*?" + end;
+		}
+
+		constexpr String STRINGS		= makePack("\"", "\"");
+		constexpr String PARENTHESES	= makePack("(", ")");
+		constexpr String ANGLE_BRACKETS	= makePack("[", "]");
+		constexpr String LINE_COMMENTS	= String("\\/\\/.*");
+		constexpr String BLOCK_COMMENTS = makePack("/*", "*/");
+
+		template<class... Args>
+		constexpr String concat(String const& first, Args const&... args)
+		requires (... && Type::Convertible<Args, String>) {
+			return first + "|" + (... + "|" + args);
+		}
+
+		constexpr String PACKS		= concat(STRINGS, PARENTHESES, ANGLE_BRACKETS, LINE_COMMENTS, BLOCK_COMMENTS);
+		constexpr String ALL_TOKENS	= concat(COMPLEX_TOKEN + "+", PACKS);
+		constexpr String ALL_TOKENS	= concat(ANY_PARAM_CHAR + "+", PACKS);
+	}
+
+	struct SyntaxTree {
+		struct ParameterPack {
+			StringList args;
+
+			constexpr ParameterPack() {}
+
+			ParameterPack(String const& packString) {	
+			}
+
+			constexpr ParameterPack(StringList const& args): args(args) {}
+
+			template<class... Args>
+			constexpr explicit ParameterPack(Args const&... args)
+			requires (... && Type::Convertible<Args, String>):
+				args(StringList{args...}) {}
+			
+			ParameterPack(ParameterPack const& other)	= default;
+			ParameterPack(ParameterPack&& other)		= default;
+		};
+
+		struct Token {
+			Operation		type;
+			String			name	= "";
+			ParameterPack	params	= ParamPack();
+			uint64			mode	= 0;
+
+			constexpr uint16 operation(uint16 const sp) {
+				if (sp)
+					return enumcast(type) | (sp << 12);
+				return enumcast(type) | (mode << 12);
+			}
+		};
+
+		using Tokens = List<Token>;
+		Tokens tokens;
+			
+		SyntaxTree(SyntaxTree const& other)	= default;
+		SyntaxTree(SyntaxTree&& other)		= default;
+
+		SyntaxTree(StringList const& nodes) {
+			if (nodes.empty())
+				throw Error::NonexistentValue("No nodes were given!", CTL_CPP_PRETTY_SOURCE);
+			for (usize i = 0; i < nodes.size(); ++i) {
+				String node = nodes[i], next;
+				if (i+1 < nodes.size())
+					next = nodes[i+1];
+				switch (node[0]) {
+					case '/': continue;
+					case '@': {
+						assertValidNamedNode(node);
+						if (next.size() && next[0] == '(') {
+							tokens.pushBack({
+								.type	= Operation::DVM_O_ACTION,
+								.name	= node.substring(1),
+								.params	= next
+							});
+							++i;
+						}
+					} break;
+					case '$': {
+						assertValidNamedNode(node);
+						String const name = node.substring(1);
+						if (next.size()) {
+							if (next[0] == '(')
+								tokens.pushBack({
+									.type	= Operation::DVM_O_NAMED_CALL,
+									.name	= name,
+									.params	= next
+								});
+							else if (next[0] == '"')
+								tokens.pushBack({
+									.type	= Operation::DVM_O_NAMED_CALL,
+									.name	= name,
+									.params	= ParameterPack(next.substring(1, next.size()-1))
+								});
+							else if (!Regex::count(next, RegexMatches::NON_NAME_CHAR))
+								tokens.pushBack({
+									.type	= Operation::DVM_O_NAMED_CALL,
+									.name	= name,
+									.params	= ParameterPack(next)
+								});
+							else throw Error::InvalidValue(
+								toString("Invalid value of '", next, "' for '", node, "'!"),
+								CTL_CPP_PRETTY_SOURCE
+							);
+						} else throw Error::InvalidValue(
+							toString("Missing value for '", node, "'!"),
+							"Maybe you confused '$' with '+' or '-', perhaps?",
+							CTL_CPP_PRETTY_SOURCE
+						);
+						++i;
+					} break;
+					case '!': {
+						assertValidNamedNode(node);
+						tokens.pushBack({
+							.type	= Operation::DVM_O_EMOTION,
+							.name	= node.substring(1)
+						});
+					} break;
+					case '\'': {
+						assertValidNamedNode(node);
+						tokens.pushBack({
+							.type	= Operation::DVM_O_EMOTION,
+							.params	= ParameterPack(node.substring(1))
+						});
+					} break;
+					case '#': {
+						
+					}
+				}
+			}
+		}
+
+	private:
+		void assertValidNamedNode(String const& node) {
+			if (node.size() < 2)
+				throw Error::InvalidValue(
+					toString("Invalid token '", node, "'!"),
+					"Token is too small!",
+					CTL_CPP_PRETTY_SOURCE
+				);
+		}
+	};
+
+	/// @brief Dialog compiler.
+	struct Compiler {
+		/// @brief Empty constructor.
+		Compiler()										{					}
+		/// @brief Compiles a dialog source.
+		/// @param source Source to compile.
+		Compiler(String const& source): source(source)	{compileScript();	}
+	
+	private:
+		/// @brief Whether the dialog source was compiled.
+		bool	compiled = false;
+		/// @brief Dialog source code.
+		String	source;
+		/// @brief Resulting compiled dialog program.
+		Dialog	binary;
+		/// @brief Current data index.
+		usize	dataIndex = 1;
+		/// @brief Values used for errors.		
+		usize	lineIndex = 1, columnIndex = 1;
+
+	};
+
 	/// @brief Dialog source compiler.
 	struct Compiler {
 		/// @brief Empty constructor.
