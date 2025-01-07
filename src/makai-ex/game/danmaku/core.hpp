321dcba921334eedb17436b2b53c45dc09dd84d9
LasagnaCake <sponge.cake@tuta.io>
2025-01-07 16:32:42 -0300

    Update core.hpp

--- src/makai-ex/game/danmaku/core.hpp
+++ src/makai-ex/game/danmaku/core.hpp
@@ -12,12 +12,32 @@ namespace Makai::Ex::Game::Danmaku {
 		float				current	= 0;
 	};
 
-	struct Pause {
+	struct PauseState {
 		llong	time	= -1;
 		bool	enabled	= false;
 	};
 
-	struct IObject: Makai::Collision::C2D::Server::ICollider {
+	struct GameObject: Makai::Collision::C2D::Server::ICollider, Makai::IUpdateable {
+		Makai::Co::Promise<usize, true> task;
+
+		PauseState pause;
+
+		void onUpdate(float delta, Makai::App& app) override {
+			if (pause.enabled && pause.time > 0) {
+				--pause.time;
+				return;
+			}
+			pause.enabled = false;
+			if (delay > 0) {
+				--delay;
+				return;
+			}
+			while (!delay && task)
+				delay = task.next();
+		}
+
+	private:
+		usize delay = 0;
 	};
 }
 
