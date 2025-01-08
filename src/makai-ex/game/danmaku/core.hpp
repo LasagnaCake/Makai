e1f6793ce2315d354ca04affd70d2fb7ad1bdf28
LasagnaCake <sponge.cake@tuta.io>
2025-01-08 10:59:44 -0300

    Update core.hpp

--- src/makai-ex/game/danmaku/core.hpp
+++ src/makai-ex/game/danmaku/core.hpp
@@ -6,7 +6,8 @@
 namespace Makai::Ex::Game::Danmaku {
 	using CollisionServer = Makai::Collision::C2D::Server;
 
-	struct Parameter {
+	struct Property {
+		bool				enabled;
 		float				from;
 		float				to;
 		float				by;
@@ -22,11 +23,16 @@ namespace Makai::Ex::Game::Danmaku {
 	struct GameObject: CollisionServer::ICollider, Makai::IUpdateable {
 		using PromiseType = Makai::Co::Promise<usize, true>;
 
+		virtual ~GameObject() {}
+
 		PromiseType task;
 
 		PauseState pause;
 
 		Math::Transform2D trans;
+		
+		virtual GameObject& spawn()		= 0;
+		virtual GameObject& despawn()	= 0;
 
 		void onUpdate(float, Makai::App&) override {
 			if (pause.enabled && pause.time > 0) {
@@ -42,8 +48,50 @@ namespace Makai::Ex::Game::Danmaku {
 				delay = task.next();
 		}
 
+		Functor<void(GameObject&, float, App&)> onObjectUpdate;
+
 	private:
 		usize delay = 0;
+	};
+
+	struct AttackObject: GameObject {
+		virtual ~AttackObject() {}
+
+		enum class State {
+			AOS_FREE,
+			AOS_SPAWNING,
+			AOS_ACTIVE,
+			AOS_DESPAWNING
+		};
+		
+		enum class Action {
+			AOA_SPAWN_BEGIN,
+			AOA_SPAWN_END,
+			AOA_DESPAWN_BEGIN,
+			AOA_DESPAWN_END,
+			AOA_UNPAUSE,
+			AOA_DISCARD
+		};
+
+		Property velocity;
+		Property rotation;
+
+		bool discardable	= true;
+		bool dope			= true;
+
+		virtual AttackObject& reset()							= 0;
+		virtual AttackObject& clear()							= 0;
+		virtual AttackObject& discard(bool const force = false)	= 0;
+
+		Functor<void(AttackObject&, Action const)>	onAction;
+
+		virtual bool isFree() = 0;
+
+		AttackObject& free()	{setFree(true);		}
+		AttackObject& enable()	{setFree(false);	}
+
+	protected:
+		virtual AttackObject& setFree(bool const state) = 0;
 	};
 }
 
