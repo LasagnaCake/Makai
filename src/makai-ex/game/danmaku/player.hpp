ec259772f90259f52eda2f1ce505e9fbcd12c088
LasagnaCake <sponge.cake@tuta.io>
2025-01-16 14:11:01 -0300

    Create player.hpp

---
+++ src/makai-ex/game/danmaku/player.hpp
@@ -0,0 +1,15 @@
+#ifndef MAKAILIB_EX_GAME_DANMAKU_PLAYER_H
+#define MAKAILIB_EX_GAME_DANMAKU_PLAYER_H
+
+#include "core.hpp"
+
+#include "../core/controlable.hpp"
+#include "../core/sprite.hpp"
+
+namespace Makai::Ex::Game::Danmaku {
+	struct Player {
+
+	};
+}
+
+#endif
