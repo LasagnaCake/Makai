e87a4d906d8fc0543286a657557cbca939c75220
LasagnaCake <sponge.cake@tuta.io>
2025-01-17 22:16:39 -0300

    Update cute.cpp

--- src/makai/impl/cute.cpp
+++ src/makai/impl/cute.cpp
@@ -1,2 +1,6 @@
 #define CUTE_SOUND_IMPLEMENTATION
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-value"
+#pragma GCC diagnostic ignored "-Wsign-compare"
 #include <cute_sound.h>
+#pragma GCC diagnostic pop
