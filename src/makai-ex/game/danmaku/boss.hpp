a2f5422cdef5c2f6ff1ab041e8c0417c72e83cae
LasagnaCake <sponge.cake@tuta.io>
2025-01-22 12:25:43 -0300

    Update boss.hpp

--- src/makai-ex/game/danmaku/boss.hpp
+++ src/makai-ex/game/danmaku/boss.hpp
@@ -6,6 +6,25 @@
 namespace Makai::Ex::Game::Danmaku {
 	struct ABoss: AEnemy {
 		ABoss(EnemyConfig const& cfg): AEnemy(cfg) {}
+
+		Graph::RadialBar healthBar;
+
+		void onUpdate(float delta) override {
+			if (!active) return;
+			AGameObject::onUpdate(delta);
+			if (paused()) return;
+			healthBar.value = health;
+			healthBar.max	= maxHealth;
+			healthBar.trans.position	= trans.position;
+			healthBar.trans.rotation.z	= trans.rotation;
+			healthBar.trans.scale		= trans.scale;
+		}
+
+		void onUpdate(float delta, App& app) override {
+			if (!active) return;
+			onUpdate(delta);
+			if (paused()) return;
+		}
 	};
 }
 
