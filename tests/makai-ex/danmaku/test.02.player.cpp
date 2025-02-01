e3198ac1e05297ccc6b03fee8e2ca148bce50466
LasagnaCake <sponge.cake@tuta.io>
2025-02-01 17:40:02 -0300

    Update test.02.player.cpp

--- tests/makai-ex/danmaku/test.02.player.cpp
+++ tests/makai-ex/danmaku/test.02.player.cpp
@@ -3,7 +3,7 @@
 
 namespace Danmaku = Makai::Ex::Game::Danmaku;
 
-constexpr Makai::Vector2 gamearea = Makai::Vector2(64, 64 * (4.0/3.0)) / 2;
+constexpr Makai::Vector2 gamearea = Makai::Vector2(64 * (4.0/3.0), 64) / 2;
 
 Danmaku::GameArea
 	board		= {gamearea * Makai::Vector2(1, -1), gamearea},
@@ -23,6 +23,7 @@ struct TestPlayer: Danmaku::APlayer {
 		input.binds["player/down"]	= {Makai::Input::KeyCode::KC_DOWN};
 		input.binds["player/left"] 	= {Makai::Input::KeyCode::KC_LEFT};
 		input.binds["player/right"]	= {Makai::Input::KeyCode::KC_RIGHT};
+		input.binds["player/focus"]	= {Makai::Input::KeyCode::KC_LEFT_SHIFT};
 		velocity = {20, 10};
 	}
 
@@ -58,11 +59,30 @@ struct TestApp: Makai::Ex::Game::App {
 
 	TestApp(): App(Makai::Config::App{{800, 600, "Test 02", false}}) {
 		loadDefaultShaders();
-		camera.cam2D = Makai::Graph::Camera3D::from2D(64, Makai::Vector2(4, 3));
+		camera.cam2D = Makai::Graph::Camera3D::from2D(32, Makai::Vector2(4, 3));
 	}
 
 	void onLayerDrawBegin(usize const layerID) override {
 		camera.use(layerID >= Danmaku::RenderLayer::BOSS1_SPELL_BG_BOTTOM_LAYER);
+	}
+
+	constexpr static usize MAX_FRCOUNT = 10;
+
+	usize frcount = 0;
+
+	float framerate[MAX_FRCOUNT];
+
+	void onUpdate(float delta) {
+		if (frcount < MAX_FRCOUNT)
+			framerate[frcount++] = 1000.0 / getFrameDelta();
+		else {
+			float fravg = 0;
+			for(float& f: framerate) fravg += f;
+			fravg *= (1.0 / (float)MAX_FRCOUNT);
+			fravg = Makai::Math::clamp<float>(fravg, 0, maxFrameRate);
+			DEBUGLN("Framerate: ", Makai::Format::prettify(Makai::Math::round(fravg, 2), 2, 0));
+			frcount = 0;
+		}
 	}
 };
 
