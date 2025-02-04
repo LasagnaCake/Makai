511dbce28fe84125c5dd099eeef84ad76f3d686d
LasagnaCake <sponge.cake@tuta.io>
2025-02-04 16:37:03 -0300

    Update test.ex.dmk.03.bullet.hpp

--- tests/makai-ex/danmaku/test.ex.dmk.03.bullet.hpp
+++ tests/makai-ex/danmaku/test.ex.dmk.03.bullet.hpp
@@ -3,10 +3,24 @@
 
 namespace Danmaku = Makai::Ex::Game::Danmaku;
 
-struct TestBulletServer: Danmaku::BulletServer<> {
-	
+constexpr Makai::Vector2 gamearea = Makai::Vector2(64 * (4.0/3.0), 64) / 2;
+
+Danmaku::GameArea
+	board		= {gamearea * Makai::Vector2(1, -1), gamearea},
+	playfield	= {gamearea * Makai::Vector2(1, -1), gamearea}
+;
+
+using BaseBulletServer = Danmaku::BulletServer<>;
+
+struct TestBulletServer: BaseBulletServer {
+	MkGraph::Renderable mesh, glowMesh;
+	Danmaku::BulletServerConfig const cfg = {1024, mesh, glowMesh, board, playfield};
+
+	TestBulletServer(): BaseBulletServer(cfg) {}
 };
 
 int main() {
+	TestBulletServer server;
+
 	return 0;
 }
