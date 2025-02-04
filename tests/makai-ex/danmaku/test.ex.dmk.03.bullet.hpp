14ed269bfd387aa1911d0663ecee3a136cdb32b6
LasagnaCake <sponge.cake@tuta.io>
2025-02-04 15:19:14 -0300

    Update test.ex.dmk.03.bullet.hpp

--- tests/makai-ex/danmaku/test.ex.dmk.03.bullet.hpp
+++ tests/makai-ex/danmaku/test.ex.dmk.03.bullet.hpp
@@ -1,7 +1,11 @@
 #include <makai/makai.hpp>
 #include <makai-ex/makai-ex.hpp>
 
+namespace Danmaku = Makai::Ex::Game::Danmaku;
 
+struct TestBulletServer: Danmaku::BulletServer {
+
+};
 
 int main() {
 	return 0;
