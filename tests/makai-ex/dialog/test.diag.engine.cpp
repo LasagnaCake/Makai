66a864c9aab14d06552762ff6481c80094225830
LasagnaCake <sponge.cake@tuta.io>
2025-02-16 20:35:05 -0300

    Stuff

--- tests/makai-ex/dialog/test.diag.engine.cpp
+++ tests/makai-ex/dialog/test.diag.engine.cpp
@@ -1,5 +1,9 @@
 #include <makai/makai.hpp>
-#include <makai-ex/dialog/dialog.hpp>
+#include <makai-ex/game/dialog/dialog.hpp>
+
+struct TestActor: Makai::Ex::Game::Dialog::Actor {
+
+};
 
 int main() {
 	return 0;
