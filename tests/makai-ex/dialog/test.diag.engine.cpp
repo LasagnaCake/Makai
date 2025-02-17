5a8f224cbc3849bd3ea8b7ec132ff7ddb7a156d9
LasagnaCake <sponge.cake@tuta.io>
2025-02-16 21:13:20 -0300

    Update test.diag.engine.cpp

--- tests/makai-ex/dialog/test.diag.engine.cpp
+++ tests/makai-ex/dialog/test.diag.engine.cpp
@@ -2,7 +2,23 @@
 #include <makai-ex/game/dialog/dialog.hpp>
 
 struct TestActor: Makai::Ex::Game::Dialog::Actor {
+	TestActor(): Actor(new Makai::Ex::Game::Dialog::Box()) {}
+};
 
+struct TestScene: Makai::Ex::Game::Dialog::Scene {
+	Makai::Instance<TestActor> actors[3] = {
+		new TestActor(),
+		new TestActor(),
+		new TestActor()
+	};
+
+	TestScene(): Scene() {
+		dialog = new Makai::Ex::Game::Dialog::Box();
+		choice = new Makai::Ex::Game::Dialog::ChoiceMenu();
+		cast[Makai::ConstHasher::hash("alice")]		= actors[0].asWeak();
+		cast[Makai::ConstHasher::hash("bob")]		= actors[1].asWeak();
+		cast[Makai::ConstHasher::hash("charlie")]	= actors[2].asWeak();
+	}
 };
 
 int main() {
