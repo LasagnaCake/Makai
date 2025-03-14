7522f84ce874f76ff72b9b2eb8f467ed914f7677
LasagnaCake <sponge.cake@tuta.io>
2025-03-14 14:01:18 -0300

    Create test.ex.dmk.06.enemy.cpp

---
+++ tests/makai-ex/danmaku/test.ex.dmk.06.enemy.cpp
@@ -0,0 +1,53 @@
+#include <makai/makai.hpp>
+#include <makai-ex/makai-ex.hpp>
+
+namespace Danmaku = Makai::Ex::Game::Danmaku;
+
+constexpr Makai::Vector2 gamearea = Makai::Vector2(64 * (4.0/3.0), 64) / 2;
+
+Danmaku::GameArea
+	board		= {gamearea * Makai::Vector2(1, -1), (gamearea)},
+	playfield	= {gamearea * Makai::Vector2(1, -1), (gamearea * 1.5)}
+;
+
+struct TestApp: Makai::Ex::Game::App {
+	Makai::Random::SecureGenerator rng;
+
+	TestApp(): App(Makai::Config::App{{800, 600, "Test 05", false}}) {
+		loadDefaultShaders();
+		camera.cam2D = Makai::Graph::Camera3D::from2D(64, Makai::Vector2(4, 3) / 3.0);
+	}
+
+	void onLayerDrawBegin(usize const layerID) override {
+		camera.use(layerID >= Danmaku::Render::Layer::BOSS1_SPELL_BG_BOTTOM_LAYER);
+	}
+
+	constexpr static usize MAX_FRCOUNT = 10;
+
+	usize frcount = 0;
+
+	float framerate[MAX_FRCOUNT];
+
+	void onUpdate(float delta) {
+		if (frcount < MAX_FRCOUNT)
+			framerate[frcount++] = 1000.0 / getCycleDelta();
+		else {
+			float fravg = 0;
+			for(float& f: framerate) fravg += f;
+			fravg *= (1.0 / (float)MAX_FRCOUNT);
+			fravg = Makai::Math::clamp<float>(fravg, 0, maxFrameRate);
+			DEBUGLN("Framerate: ", Makai::Format::prettify(Makai::Math::round(fravg, 2), 2, 0));
+			frcount = 0;
+		}
+	}
+};
+
+int main() {
+	try {
+		TestApp app;
+		app.run();
+	} catch (Makai::Error::Generic const& e) {
+		Makai::Popup::showError(e.what());
+	}
+	return 0;
+}
