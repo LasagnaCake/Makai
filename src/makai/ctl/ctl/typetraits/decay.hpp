e68a242e9fc8bd977cd282ec03563fc757574250
LasagnaCake <80758022+LasagnaCake@users.noreply.github.com>
2025-03-18 20:04:24 -0300

    Update decay.hpp

--- src/makai/ctl/ctl/typetraits/decay.hpp
+++ src/makai/ctl/ctl/typetraits/decay.hpp
@@ -74,13 +74,13 @@ constexpr AsConstant<T>&		constant(T& v)					{return v;										}
 /// @param v Value to decay.
 /// @return Value as either a reference or temporary.
 template<class T>
-constexpr T&& forward(T& v)				{return v;			}
+constexpr T&& forward(T& v)		{return v;			}
 
 /// @brief Forwards references as either references or temporaries, depending on its type.
 /// @param v Value to decay.
 /// @return Value as either a reference or temporary.
 template<class T>
-constexpr T&& forward(T&& v)			{return move(v);	}
+constexpr T&& forward(T&& v)	{return move(v);	}
 
 CTL_NAMESPACE_END
 
