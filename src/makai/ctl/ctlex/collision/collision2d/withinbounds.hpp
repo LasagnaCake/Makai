abc16d7fdbbcbbf0d40f8632e6b70f18f7062b12
LasagnaCake <sponge.cake@tuta.io>
2025-04-06 00:18:19 -0300

    Update withinbounds.hpp

--- src/makai/ctl/ctlex/collision/collision2d/withinbounds.hpp
+++ src/makai/ctl/ctlex/collision/collision2d/withinbounds.hpp
@@ -16,17 +16,37 @@ namespace Collision::C2D {
 		;
 	}
 
+	/// @brief Checks whether two points collide.
+	/// @param pointA Point to check.
+	/// @param pointB Point to check against.
+	/// @return Whether points collide.
+	constexpr bool withinBounds(Vector2 const& pointA, Vector2 const& pointB) {
+		return pointA == pointB;
+	}
+
+	/// @brief Checks whether a point collides with a bound.
+	/// @param point Point to check.
+	/// @param area Bound to check against.
+	/// @return Whether collision happens.
 	constexpr bool withinBounds(Vector2 const& point, IBound2D const& area) {
 		auto p = Point(point);
 		return GJK::check(p, area);
 	}
 
+	/// @brief Checks whether a bound collides with a point.
+	/// @param area Bound to check.
+	/// @param point Point to check against.
+	/// @return Whether collision happens.
 	constexpr bool withinBounds(IBound2D const& area, Vector2 const& point) {
 		return withinBounds(point, area);
 	}
 
+	/// @brief Checks whether two bounds collide.
+	/// @param area Bound to check.
+	/// @param point Bound to check against.
+	/// @return Whether collision happens.
 	constexpr bool withinBounds(IBound2D const& a, IBound2D const& b) {
-		return GJK::check(a, b);
+		return GJK::check(a, b) || GJK::check(b, a);
 	}
 	
 	static_assert(Point(0).bounded(Point(0)));
@@ -46,6 +66,7 @@ namespace Collision::C2D {
 	static_assert(withinBounds(Point(0 + 64), Point(0 + 64)));
 	static_assert(withinBounds(Point(0 + 64), Circle(0.5 + 64, 1)));
 	static_assert(withinBounds(Point(0 + 64), Box(0 + 64, 1)));
+	// FIXME: Succeeds when closer to the origin, but fails when far away >:(
 	static_assert(withinBounds(Circle(1 + 64, 2), Circle(0 + 64, 2)));
 	static_assert(withinBounds(Circle(1 + 64, 2), Box(0 + 64, 2)));
 	static_assert(withinBounds(Box(0.25 + 64, 1), Box(-0.25 + 64, 1)));
