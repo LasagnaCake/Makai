00394ee08d5c8af7be59517b464d350c15bb4f28
LasagnaCake <sponge.cake@tuta.io>
2025-04-23 16:04:51 -0300

    Update choicemenu.hpp

--- src/makai-ex/game/dialog/choicemenu.hpp
+++ src/makai-ex/game/dialog/choicemenu.hpp
@@ -7,12 +7,17 @@
 
 /// @brief Dialog facilities.
 namespace Makai::Ex::Game::Dialog {
+	/// @brief Choice menu.
 	struct ChoiceMenu: IVisible, AUpdateable, Controllable {
+		/// @brief Options display.	
 		Graph::Label menu;
+		/// @brief Selection cursor display.
 		Graph::Label cursor;
 
+		//// @brief Destructor.
 		virtual ~ChoiceMenu() {}
 
+		/// @brief Default constructor.
 		ChoiceMenu() {
 			bindmap = Dictionary<String>({	
 				{"next",		"dialog/choice/next"		},
@@ -22,6 +27,7 @@ namespace Makai::Ex::Game::Dialog {
 			});
 		}
 
+		/// @brief Gets called every execution cycle.
 		void onUpdate(float, Makai::App&) override {
 			if (!updating) return;
 			if (counter < cooldown) {
@@ -39,17 +45,18 @@ namespace Makai::Ex::Game::Dialog {
 			}
 		}
 
+		/// @brief Sets the curently-highlighted choice.
+		/// @param newChoice Choice to select.
 		void setChoice(ssize const newChoice) {
 			if (prevChoice == newChoice) return;
 			choice = newChoice;
-			while (choice < 0)
-				choice += options.size();
-			while (choice >= static_cast<ssize>(options.size()))
-				choice -= options.size();
+			choice = Math::wrapi<ssize>(choice, 0, options.size());
 			onFocusChange(prevChoice, choice);
 			prevChoice = choice;
 		}
-
+		
+		/// @brief Sets the list of choices to display.
+		/// @param choices Choice list.
 		void setOptions(StringList const& choices) {
 			options		= choices;
 			choice		= 0;
@@ -58,45 +65,61 @@ namespace Makai::Ex::Game::Dialog {
 			onOptionsChanged();	
 		}
 
+		/// @brief Returns the current choice list.
+		/// @return Current choice list.
 		StringList const& getOptions() {
 			return options;
 		}
 
+		/// @brief Clears the current selection.
 		void clear()	{posted = false; cooldown = 0;													}
+		/// @brief Selects the currently-highlighted choice.
 		void select()	{posted = true; hide();															}
+		/// @brief If `exitOnCancel` is true, selects -1. Else, highlights the last choice in the list.
 		void cancel()	{if (exitOnCancel) {choice = -1; select();} else setChoice(options.size()-1);	}
 
-		virtual void onFocusChange(ssize const oldChoice, ssize const newChoice) {
-			repaint();
-		}
+		/// @brief Called when the currently-highlighted choice is changed.
+		virtual void onFocusChange(ssize const oldChoice, ssize const newChoice) {repaint();}
 
-		virtual void onOptionsChanged()	{
-			repaint();
-		}
+		/// @brief Called when the list of options is changed.
+		virtual void onOptionsChanged()	{repaint();}
 
+		/// @brief Shows & enables the choice menu.
 		void show() override {
 			menu.active		=
 			cursor.active	=
 			updating		= true;
+			counter			= 0;
 		}
 
+		/// @brief Hides & disables the choice menu.
 		void hide() override {
 			menu.active		=
 			cursor.active	=
 			updating		= false;
+			counter			= 0;
 		}
 
+		/// @brief Returns whether a choice is selected.
 		bool ready()	{return posted;						}
+		/// @brief Returns the currently-selected choice.
+		/// @return Currently-selected choice.
 		ssize value()	{return choice;						}
+		/// @brief Collects & clears the currently-selected choice.
+		/// @return Currently-selected choice.
 		ssize collect()	{posted = false; return value();	}
 
+		/// @brief Time to wait before starting to respond to user input.
 		usize cooldown = 1;
 
+		/// @brief Whether cancelling exits the choice, or highlights the last option.
 		bool exitOnCancel = false;
 
 	private:
+		/// @brief Counter for timing purposes.
 		usize counter = 0;
 
+		/// @brief Whether an option was selected.
 		bool posted = false;
 
 		void repaint() {
@@ -136,9 +159,12 @@ namespace Makai::Ex::Game::Dialog {
 			display += "> " + String(cursor.text->rect.h - 4, ' ') + " <";
 		}
 
+		/// @brief Options to display.
 		StringList options;
 
+		/// @brief Currently-highlighted choice.
 		ssize choice		= 0;
+		/// @brief Previously-highlighted choice.
 		ssize prevChoice	= 0;
 	};
 }
