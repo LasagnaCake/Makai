b3ee6e9694152717e6ac3ed9c0124a63509e30ca
LasagnaCake <sponge.cake@tuta.io>
2025-04-27 10:35:32 -0300

    Create spawner.hpp

---
+++ src/makai-ex/game/danmaku/anima/spawner.hpp
@@ -0,0 +1,11 @@
+#ifndef MAKAILIB_EX_GAME_DANMAKU_ANIMA_SERVERSPAWNER_H
+#define MAKAILIB_EX_GAME_DANMAKU_ANIMA_SERVERSPAWNER_H
+
+#include "../server.hpp"
+
+/// @brief Anima-specific danmaku facilities.
+namespace Makai::Ex::Game::Danmaku::Anima {
+	
+}
+
+#endif
