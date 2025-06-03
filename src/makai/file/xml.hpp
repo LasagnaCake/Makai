5feb6fe773448801e52e718645268febdd73df90
LasagnaCake <sponge.cake@tuta.io>
2025-06-02 21:49:02 -0300

    Update xml.hpp

--- src/makai/file/xml.hpp
+++ src/makai/file/xml.hpp
@@ -10,6 +10,15 @@ namespace Makai::XML {
 	/// @param xml String to convert.
 	/// @return String as JSON value.
 	/// @throw Error::FailedAction at XML and JSON parsing failures.
+	/// @note
+	///		Tags are converted to JSON objects, where its accessor key is the tag name.
+	///		
+	///		All tag attributes are prefixed with an `@` in the resulting JSON, and contained within the tag object.
+	///		
+	///		Rogue text in a tag is located in the `.content` property.
+	///		
+	///		If there is only one tag of a given type, it results in a single object.
+	///		If there is more than one tag of a given type, it results in an array of objects.
 	JSON::JSONValue toJSON(String const& xml);
 }
 
