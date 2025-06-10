ebdf430697609e65bd1b577995aa9a15a88b3a68
LasagnaCake <sponge.cake@tuta.io>
2025-06-10 20:18:29 -0300

    Other stuff

--- tests/makai/test.06.armature.cpp
+++ tests/makai/test.06.armature.cpp
@@ -22,7 +22,7 @@ struct TestApp: Makai::App {
 
 	void animateCube() {
 		float as, ac;
-		Makai::Math::absincos<float>((getCurrentCycle() / 15.0), as, ac);
+		Makai::Math::absincos<float>((getCurrentCycle() / 30.0), as, ac);
 		danceCube.armature.pose[1].rotation.x = -ac * 0.2 * Makai::Math::Constants::PI;
 		danceCube.armature.pose[2].rotation.x = ac * 0.2 * Makai::Math::Constants::PI;
 		danceCube.armature.pose[3].rotation.x = as * 0.2 * Makai::Math::Constants::PI;
@@ -32,7 +32,7 @@ struct TestApp: Makai::App {
 	void onUpdate(float delta) override {
 		float as, ac;
 		Makai::Math::sincos<float>((getCurrentCycle() / 180.0), as, ac);
-		camera.eye	= Makai::Vec3(as, 0.1, ac) * 5.0;
+		camera.eye	= Makai::Vec3(as, 0.25, ac) * 5.0;
 	}
 };
 
