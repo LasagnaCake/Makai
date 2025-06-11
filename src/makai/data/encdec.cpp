abc62dff6ca427a940636318d85f706081d63746
LasagnaCake <sponge.cake@tuta.io>
2025-06-10 21:55:39 -0300

    Update encdec.cpp

--- src/makai/data/encdec.cpp
+++ src/makai/data/encdec.cpp
@@ -10,8 +10,8 @@ using namespace Data;
 BinaryData<> Data::decode(String const& data, EncodingType const& encoding) try {
 	std::vector<ubyte> result;
 	switch (encoding) {
-		case EncodingType::ET_BASE32: result = cppcodec::base32_rfc4648::decode<std::vector<ubyte>>(data);
-		case EncodingType::ET_BASE64: result = cppcodec::base64_rfc4648::decode<std::vector<ubyte>>(data);
+		case EncodingType::ET_BASE32: result = cppcodec::base32_rfc4648::decode<std::vector<ubyte>>(data); break;
+		case EncodingType::ET_BASE64: result = cppcodec::base64_rfc4648::decode<std::vector<ubyte>>(data); break;
 		default: throw Error::InvalidValue("Invalid encoding type!", CTL_CPP_PRETTY_SOURCE);
 	}
 	return BinaryData<>(&*result.begin(), &*result.end());
