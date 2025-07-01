0780012a3f4910dee14a3db6cfaf339ef2e1c9b6
LasagnaCake <sponge.cake@tuta.io>
2025-06-30 23:56:00 -0300

    Update xml.cpp

--- src/makai/file/xml.cpp
+++ src/makai/file/xml.cpp
@@ -2,13 +2,25 @@
 #include "json.hpp"
 
 #include <exception>
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+// SHUT UUUUUUUUUUUUUUP
 #pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+#pragma GCC diagnostic ignored "-Woverflow"
+
 #define XML2JSON_CONTENT_PROP		".content"
 #define XML2JSON_ATTRIB_PREFIX		"@" 
 #define XML2JSON_ACCEPTS_NUMBERS	true
 #include <xml2json/xml2json.hpp>
+
 #pragma GCC diagnostic pop
 
 Makai::JSON::JSONValue Makai::XML::toJSON(Makai::String const& xml) {
