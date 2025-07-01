6635d00c43d3ce14d558c96677f9836f5d5c73b3
LasagnaCake <sponge.cake@tuta.io>
2025-06-30 22:47:16 -0300

    Documentation

--- src/makai/ctl/ctl/range/iterate.hpp
+++ src/makai/ctl/ctl/range/iterate.hpp
@@ -5,19 +5,37 @@
 
 CTL_NAMESPACE_BEGIN
 
+/// @brief Range-lated facilites.
 namespace Range {
+	/// @brief Implementation details.
 	namespace Impl {
+		/// @brief Iterator wrapper.
+		/// @tparam T Iterator type.
 		template <class T>
 		struct IteratorWrapper {
+			/// @brief Constructs the iterator wrapper.
+			/// @param begin Iterator to beginning of range.
+			/// @param end Iterator to end of range.
 			constexpr IteratorWrapper(T const& begin, T const& end): from(begin), to(end) {}
+			/// @brief Returns the iterator to the beginning of the range.
+			/// @return Iterator to beginning of range.
 			constexpr T begin() const	{return from;	}
+			/// @brief Returns the iterator to the end of the range.
+			/// @return Iterator to beginning of range.
 			constexpr T end() const		{return to;		}
 		private:
+			/// @brief Start of range.
 			T const from;
+			/// @brief End of range.
 			T const to;
 		};
 	}
 
+	/// @brief Wraps an iterator pair for easy range-based for loop iteration.
+	/// @tparam T Iterator type.
+	/// @param begin Iterator to beginning of range.
+	/// @param end Iterator to end of range.
+	/// @return Wrapped iterators.
 	template<class T>
 	constexpr Impl::IteratorWrapper<T> iterate(T const& begin, T const& end) {
 		return {begin, end};
--- src/makai/ctl/ctl/range/sequence.hpp
+++ src/makai/ctl/ctl/range/sequence.hpp
@@ -8,44 +8,79 @@
 
 CTL_NAMESPACE_BEGIN
 
+/// @brief Range-lated facilites.
 namespace Range {
+	/// @brief Implementation details.
 	namespace Impl {
+		/// @brief Sequence iterator.
+		/// @tparam T Number type.
 		template<Type::Number T>
 		struct SequenceIterator {
+			/// @brief Number type.
 			using DataType			= T;
+			/// @brief Number difference type.
 			using DifferenceType	= Meta::DualType<Type::Integer<DataType>, AsSigned<DataType>, DataType>;
 
-			constexpr SequenceIterator(DataType const value, DifferenceType const& step):
+			/// @brief Constructs the iterator with a starting value, and a stride.
+			/// @param value Starting value.
+			/// @param stride Stride between steps.
+			constexpr SequenceIterator(DataType const value, DifferenceType const& stride):
 				value(value),
-				step(step),
-				decrementing(step < 0) {
-					if (!step) throw InvalidValueException("Step cannot be zero!");
+				stride(stride),
+				decrementing(stride < 0) {
+					if (!stride) throw InvalidValueException("Step cannot be zero!");
 				}
 
+			/// @brief Dereference operator overloading.
 			constexpr DataType operator*() const			{return value;					}
-			constexpr SequenceIterator& operator++() const	{value += step; return *this;	}
-			constexpr SequenceIterator& operator--() const	{value -= step; return *this;	}
+			/// @brief Pre-increment operator overloading.
+			/// @return Reference to self.
+			constexpr SequenceIterator& operator++() const	{value += stride; return *this;	}
+			/// @brief Pre-decrement operator overloading.
+			/// @return Reference to self.
+			constexpr SequenceIterator& operator--() const	{value -= stride; return *this;	}
 
+			/// @brief Returns whether one iterator has overshot another iterator.
+			/// @return Whether overshot happened.
 			constexpr bool operator!=(SequenceIterator const& other) const {
-				return decrementing ? value > other.value : value < other.value;
+				return decrementing
+				?	(value > other.value || (value - stride) > other.value)
+				:	(value < other.value || (value - stride) < other.value)
+				;
 			}
 
 		private:
+			/// @brief Current number.
 			DataType				value;
-			DifferenceType const	step;
+			/// @brief Stride between steps.
+			DifferenceType const	stride;
+			/// @brief Whether iterator is incrementing or decrementing.
 			bool const				decrementing;
 		};
 
+		/// @brief Wrapper for `IteratorSequence`.
+		/// @tparam T Number type.
 		template<Type::Number T>
 		using SequenceWrapper = IteratorWrapper<SequenceIterator<T>>;
 	}
 
+	/// @brief returns a numeric sequence from `begin` to `end` with steps of `step`.
+	/// @tparam T Number type.
+	/// @param begin Starting value.
+	/// @param end End value.
+	/// @param step Stride between steps.
+	/// @return Sequence.
 	template<Type::Number T>
 	constexpr Impl::SequenceWrapper<T> sequence(T const begin, T const end, T const step) {
 		if (!step) throw InvalidValueException("Step cannot be zero!");
 		return {{begin, step}, {end, step}};
 	}
 
+	/// @brief returns a numeric sequence from `begin` to `end`.
+	/// @tparam T Number type.
+	/// @param begin Starting value.
+	/// @param end End value.
+	/// @return Sequence.
 	template<Type::Number T>
 	constexpr Impl::SequenceWrapper<T> sequence(T const begin, T const end) {
 		if (begin < end)
@@ -53,22 +88,41 @@ namespace Range {
 		return sequence<T>(begin, end, -1);
 	}
 
+	/// @brief returns a numeric sequence from `0` to `end`.
+	/// @tparam T Number type.
+	/// @param end End value.
+	/// @return Sequence.
 	template<Type::Number T>
 	constexpr Impl::SequenceWrapper<T> sequence(T const end) {
 		return sequence<T>(0, end);
 	}
 }
 
+/// @brief returns a numeric sequence from `begin` to `end` with steps of `step`.
+/// @tparam T Number type.
+/// @param begin Starting value.
+/// @param end End value.
+/// @param step Stride between steps.
+/// @return Sequence.
 template<Type::Number T>
 constexpr auto range(T const begin, T const end, T const step) {
 	return Range::sequence<T>(begin, end, step);
 }
 
+/// @brief returns a numeric sequence from `begin` to `end`.
+/// @tparam T Number type.
+/// @param begin Starting value.
+/// @param end End value.
+/// @return Sequence.
 template<Type::Number T>
 constexpr auto range(T const begin, T const end) {
 	return Range::sequence<T>(begin, end);
 }
 
+/// @brief returns a numeric sequence from `0` to `end`.
+/// @tparam T Number type.
+/// @param end End value.
+/// @return Sequence.
 template<Type::Number T>
 constexpr auto range(T const end) {
 	return Range::sequence<T>(0, end);
