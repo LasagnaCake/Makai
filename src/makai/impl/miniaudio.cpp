ab9d36fec645736676a5c01d365272f2b998615b
LasagnaCake <sponge.cake@tuta.io>
2025-07-28 20:41:20 -0300

    Update miniaudio.cpp

--- src/makai/impl/miniaudio.cpp
+++ src/makai/impl/miniaudio.cpp
@@ -1,6 +1,9 @@
-#define MA_IMPLEMENTATION
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
+#include <miniaudio.h>
+#include <miniaudio_vorbis.h>
+#define MA_IMPLEMENTATION
+#define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 #include <miniaudio_vorbis.h>
 #pragma GCC diagnostic pop
