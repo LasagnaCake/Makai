4eaeda63e4a368bb782afaecf7a2c8f820bba2d4
LasagnaCake <sponge.cake@tuta.io>
2025-10-27 02:10:14 -0300

    Update jsonparser.hpp

--- src/makai/parser/data/jsonparser.hpp
+++ src/makai/parser/data/jsonparser.hpp
@@ -68,13 +68,13 @@ namespace Makai::Parser::Data {
 					auto const obj = parseObject();
 					if (obj)
 						result[result.size()] = obj.value();
-					else return obj.error().value();
+					else return obj.error().orElse({.what = "Unknown object error!"});
 				} break;
 				case TokenType{'['}: {
 					auto const obj = parseArray();
 					if (obj)
 						result[result.size()] = obj.value();
-					else return obj.error().value();
+					else return obj.error().orElse({.what = "Unknown array error!"});
 				} break;
 				case TokenType::LTS_TT_IDENTIFIER: {
 					auto const id = token.value.get<Value::StringType>();
@@ -123,13 +123,13 @@ namespace Makai::Parser::Data {
 						auto const obj = parseObject();
 						if (obj)
 							result[key] = obj.value();
-						else return obj.error().value();
+						else return obj.error().orElse({.what = "Unknown object error!"});
 					} break;
 					case TokenType{'['}: {
 						auto const obj = parseArray();
 						if (obj)
 							result[key] = obj.value();
-						else return obj.error().value();
+						else return obj.error().orElse({.what = "Unknown array error!"});
 					} break;
 					case TokenType::LTS_TT_IDENTIFIER: {
 						auto const id = token.value.get<Value::StringType>();
