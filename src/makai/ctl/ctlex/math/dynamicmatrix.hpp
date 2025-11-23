b3123eccf5aed9db2fb2522d375eb87cf61b854a
LasagnaCake <sponge.cake@tuta.io>
2025-11-22 21:57:39 -0300

    Update dynamicmatrix.hpp

--- src/makai/ctl/ctlex/math/dynamicmatrix.hpp
+++ src/makai/ctl/ctlex/math/dynamicmatrix.hpp
@@ -50,6 +50,30 @@ struct DynamicMatrix {
 		}
 	}
 
+	/// @brief Constructs a dynamic matrix from a `Vector2`.
+	/// @param vec Vector to construct from.
+	constexpr DynamicMatrix(Vector2 const& vec): DynamicMatrix(2, 1) {
+		at(0)[0] = vec.x;
+		at(0)[1] = vec.y;
+	}
+
+	/// @brief Constructs a dynamic matrix from a `Vector3`.
+	/// @param vec Vector to construct from.
+	constexpr DynamicMatrix(Vector3 const& vec): DynamicMatrix(3, 1) {
+		at(0)[0] = vec.x;
+		at(0)[1] = vec.y;
+		at(0)[2] = vec.z;
+	}
+
+	/// @brief Constructs a dynamic matrix from a `Vector4`.
+	/// @param vec Vector to construct from.
+	constexpr DynamicMatrix(Vector4 const& vec): DynamicMatrix(4, 1) {
+		at(0)[0] = vec.x;
+		at(0)[1] = vec.y;
+		at(0)[2] = vec.z;
+		at(0)[3] = vec.w;
+	}
+
 	/// @brief Returns the row at the given column.
 	/// @param index Index to match.
 	/// @return Row view.
@@ -80,7 +104,7 @@ struct DynamicMatrix {
 	/// @param index Index to match.
 	/// @return Row view.
 	/// @throws Error::InvalidValue if `index` is bigger than column count.
-	constexpr SingleColumnType<DataType> operator[](usize const index)  {
+	constexpr SingleColumnType<DataType> operator[](usize const index) {
 		return at(index);
 	}
 
@@ -105,9 +129,82 @@ struct DynamicMatrix {
 	/// @return Iterator to end of matrix.
 	constexpr auto end() const		{return matrix.end();	}
 
+	/// @brief Returns the amount of cells in the matrix.
+	/// @return Cell count.
+	constexpr auto size() const			{return matrix.size();	}
+	
+	/// @brief Returns whether the matrix does not contain cells.
+	/// @return Whether matrix does not contain cells.
+	constexpr bool empty() const		{return matrix.empty();	}
+
+	/// @brief Returns the amount of cells in each row.
+	/// @return Row size.
+	constexpr usize rowSize() const		{return rows;			}
+	/// @brief Returns the amount of cells in each column.
+	/// @return Column size.
+	constexpr usize columnSize() const	{return columns;		}
+
+	/// @brief Return the last column of the matrix as a `Vector2`.
+	/// @return Last column.
+	/// @throw Error::FailedAction if row count is less than 2.
+	constexpr Vector2 toVector2() const {
+		if (rows < 2)
+			throw Error::FailedAction("Matrix is not a valid representation of a 2D vector!", CTL_CPP_PRETTY_SOURCE);
+		if (rows == 4) return Vector2((*this)[columns-1][0], (*this)[columns-1][1]) / (*this)[columns-1][3];
+		else return Vector2((*this)[columns-1][0], (*this)[columns-1][1]);
+	}
+
+	/// @brief Return the last column of the matrix as a `Vector3`.
+	/// @return Last column.
+	/// @throw Error::FailedAction if row count is less than 3.
+	constexpr Vector3 toVector3() const {
+		if (rows < 3)
+			throw Error::FailedAction("Matrix is not a valid representation of a 3D vector!", CTL_CPP_PRETTY_SOURCE);
+		if (rows == 4) return Vector3((*this)[columns-1][0], (*this)[columns-1][1], (*this)[columns-1][2]) / (*this)[columns-1][3];
+		else return Vector3((*this)[columns-1][0], (*this)[columns-1][1], (*this)[columns-1][2]);
+	}
+
+	/// @brief Return the last column of the matrix as a `Vector4`.
+	/// @return Last column.
+	/// @throw Error::FailedAction if row count is less than 4.
+	constexpr Vector4 toVector4() const {
+		if (rows < 4)
+			throw Error::FailedAction("Matrix is not a valid representation of a 4D vector!", CTL_CPP_PRETTY_SOURCE);
+		return Vector4((*this)[columns-1][0], (*this)[columns-1][1], (*this)[columns-1][2], (*this)[columns-1][3]);
+	}
+
+	constexpr explicit operator Vector2() const {return toVector2();}
+	constexpr explicit operator Vector3() const {return toVector3();}
+	constexpr explicit operator Vector4() const {return toVector4();}
+
+	constexpr DynamicMatrix operator+(DynamicMatrix const& other) const {
+		if (!canAddOrSubtractWith(other))
+			throw Error::FailedAction(
+				"Cannot add matrices of different sizes!",
+				toString(
+					"Matrix A(r, c) -> [",rows,", ",columns,"]\n"
+					"Matrix B(r, c) -> [",other.rows,", ",other.columns,"]"
+				)
+			);
+		DynamicMatrix result = DynamicMatrix(rows, columns);
+		for (usize i = 0; i < size(); ++i)
+			result.matrix[i] = matrix[i] + other.matrix[i];
+	}
+
+	constexpr bool canAddOrSubtractWith(DynamicMatrix const& other) const {
+		return size() == other.size();
+	}
+
+	constexpr bool canMultiplyWith(DynamicMatrix const& other) const {
+		return rows == other.columns;
+	}
+
 private:
-	usize			rows;
-	usize			columns;
+	/// @brief Row count.
+	usize			rows	= 0;
+	/// @brief Column count.
+	usize			columns	= 0;
+	/// @brief underlying matrix.
 	MatrixType<>	matrix;
 };
 
