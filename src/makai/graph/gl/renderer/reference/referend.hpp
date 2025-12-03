37d8bd7b5b5e32131d37dbca58482938ff71ff0a
LasagnaCake <sponge.cake@tuta.io>
2025-12-03 19:22:35 -0300

    New graphical reference subsystem

---
+++ src/makai/graph/gl/renderer/reference/referend.hpp
@@ -0,0 +1,99 @@
+#ifndef MAKAILIB_GRAPH_RENDERER_REFERENCE_REFEREND_H
+#define MAKAILIB_GRAPH_RENDERER_REFERENCE_REFEREND_H
+
+#include "reference.hpp"
+#include "shape.hpp"
+
+namespace Makai::Graph::Ref {
+	struct Referend {
+		/// @brief Triangle storage container type.
+		using TriangleBank = List<Triangle>;
+
+		/// @brief Constructs the reference holder.
+		/// @param triangles Reference to triangle bank.
+		/// @param lockState Reference to lock state.
+		Referend(
+			TriangleBank& triangles,
+			bool& lockState
+		): triangles(triangles), lockState(lockState) {}
+
+		/// @brief Destructor.
+		virtual ~Referend() {
+			clearReferences();
+		}
+
+		/// @brief Creates a shape reference bound to this object.
+		/// @tparam T Reference type.
+		/// @return Reference instance.
+		template<AShapeType T>
+		[[nodiscard]]
+		Instance<T> createReference() {
+			constexpr usize count = T::SIZE;
+			if (lockState) throw Error::InvalidAction("Renderable object is locked!", CTL_CPP_PRETTY_SOURCE);
+			triangles.expand(count, {});
+			// Create shape
+			T* shape = new T({triangles, triangles.size()-count, count}, *this);
+			// Add to reference list
+			references.pushBack(shape);
+			// return shape
+			return shape;
+		}
+
+		/// @brief
+		///		Destroys a reference and its associated triangles.
+		///		Will only execute if reference is associated with this object.
+		/// @tparam T Reference type.
+		/// @param ref Reference to remove.
+		/// @note If successful, also destroys the reference.
+		template <AShapeType T>
+		void removeReference(Instance<T> ref) {
+			if (!ref) return;
+			if (lockState) return;
+			removeReference(*ref);
+			auto const rp = ref.raw();
+			ref.release();
+			delete rp;
+		}
+
+		/// @brief
+		///		Destroys a reference while keeping its associated triangles.
+		///		Will only execute if reference is associated with this object.
+		/// @tparam T Reference type.
+		/// @param ref Reference to remove.
+		/// @note If successful, also destroys the reference.
+		template <AShapeType T>
+		void unbindReference(Instance<T> ref) {
+			if (!ref) return;
+			if (lockState) return;
+			unbindReference(*ref);
+			auto const rp = ref.raw();
+			ref.release();
+			delete rp;
+		}
+
+		/// @brief Destroys all bound references.
+		void clearReferences();
+
+	protected:
+		/// @brief Transforms all bound references.
+		void transformReferences();
+		/// @brief Resets applied transformation in all bound references.
+		void resetReferenceTransforms();
+
+	private:
+		void removeReference(IReference& ref);
+		void unbindReference(IReference& ref);
+
+		/// @brief References bound to this object.
+		List<IReference*>	references;
+
+		friend class IReference;
+
+		/// @brief Triangle bank to store triangles into. 
+		TriangleBank&	triangles;
+		/// @brief Triangle bank lock state.
+		bool&			lockState;
+	};
+}
+
+#endif
---
+++ src/makai/graph/gl/renderer/reference/shape.hpp
@@ -0,0 +1,50 @@
+#ifndef MAKAILIB_GRAPH_RENDERER_REFERENCE_SHAPE_H
+#define MAKAILIB_GRAPH_RENDERER_REFERENCE_SHAPE_H
+
+#include "core.hpp"
+
+namespace Makai::Graph::Ref {
+	/// @brief Generic shape reference.
+	/// @tparam N Triangle count.
+	template<usize N>
+	struct AShape: public IReference {
+		/// @brief Triangle count.
+		constexpr static usize SIZE = N;
+
+		static_assert(N > 0, "Empty shapes are invalid!");
+
+		/// @brief Constructs the reference.
+		/// @param triangles Triangles bound to the reference.
+		/// @param parent Parent renderable object.
+		AShape(
+			BoundRange const& triangles,
+			Referend& parent
+		): IReference(triangles, parent) {}
+
+		/// @brief Destructor.
+		virtual ~AShape() {}
+
+		/// @brief Resets transformations applied to the bound triangles. Must be implemented.
+		/// @return Handle to self.
+		virtual Handle<IReference> reset()		= 0;
+		/// @brief Applies transformations to the bound triangles. Must be implemented.
+		/// @return Handle to self.
+		virtual Handle<IReference> transform()	= 0;
+
+		/// @brief Whether transformations should be applied.
+		bool fixed		= true;
+		/// @brief Whether the reference is visible.
+		bool visible	= true;
+
+		/// @brief Transformation.
+		Transform3D local;
+	};
+
+	/// @brief Type must be a shape reference some kind.
+	template<class T>
+	concept AShapeType = requires {
+		T::SIZE;
+	} && Makai::Type::Derived<T, AShape<T::SIZE>> && NotEmpty<T>;
+}
+
+#endif
