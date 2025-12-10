e5810bb1ee5d83d91e6e91be05b0335b554c6ded
LasagnaCake <sponge.cake@tuta.io>
2025-12-10 10:15:59 -0300

    Update referend.cpp

--- src/makai/graph/gl/renderer/reference/referend.cpp
+++ src/makai/graph/gl/renderer/reference/referend.cpp
@@ -9,6 +9,7 @@ Referend::~Referend() {
 void Referend::removeReference(AReference& ref)  {
 	if (lockState) return;
 	if (references.find(&ref) == -1) return;
+	if (!ref.triangles) return;
 	triangles.removeRange(ref.triangles.start, ref.triangles.start + ref.triangles.count);
 	unbindReference(ref);
 }
