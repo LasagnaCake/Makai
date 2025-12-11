0f9e127ab6bc91d939803bd56f945aefff78e6ff
LasagnaCake <sponge.cake@tuta.io>
2025-12-11 14:53:03 -0300

    Update srptransform.cc

--- src/makai/graph/gl/renderer/reference/srptransform.cc
+++ src/makai/graph/gl/renderer/reference/srptransform.cc
@@ -1,12 +1,8 @@
 #include "core.hpp"
 
 inline void srpTransform(Makai::Graph::Vertex& vtx, Makai::Matrix4x4 const& tmat, Makai::Matrix3x3 const& nmat) {
-	// Position
-	Makai::Vector3 res = (tmat * Makai::Vector4(vtx.position, 1.0f)).toVector3();
-	vtx.position = (res);
-	// Normal
-	res = (tmat * Makai::Vector4(vtx.normal, 1.0f)).toVector3();
-	vtx.normal = (res);
+	vtx.position = (tmat * Makai::Vector4(vtx.position, 1.0f)).toVector3();
+	vtx.normal = nmat * vtx.normal;
 }
 
 inline void srpTransform(Makai::Graph::Vertex& vtx, Makai::Transform3D const& trans) {
