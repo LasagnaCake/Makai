7a53a103672c99a7639994468f9e87cbfb38cca3
LasagnaCake <sponge.cake@tuta.io>
2025-12-29 02:41:49 -0300

    Update version.hpp

--- src/makai/ctl/ctlex/data/version.hpp
+++ src/makai/ctl/ctlex/data/version.hpp
@@ -22,10 +22,10 @@ namespace Data {
 					.split('.')
 					.filter([] (auto& e) {return !e.isNullOrSpaces();})
 				;
-				if (verv.size() > 0) ver.major		= toUInt64(verv[0]);
-				if (verv.size() > 1) ver.minor		= toUInt64(verv[1]);
-				if (verv.size() > 2) ver.patch		= toUInt64(verv[2]);
-				if (verv.size() > 3) ver.hotfix		= toUInt64(verv[3]);
+				if (verv.size() > 0) ver.major		= toUInt64(verv[0], 10);
+				if (verv.size() > 1) ver.minor		= toUInt64(verv[1], 10);
+				if (verv.size() > 2) ver.patch		= toUInt64(verv[2], 10);
+				if (verv.size() > 3) ver.hotfix		= toUInt64(verv[3], 10);
 			}
 			return ver;
 		}
