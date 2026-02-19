97c11b41309e828650b81b8f372121f36c6e3517
LasagnaCake <sponge.cake@tuta.io>
2026-02-19 11:58:50 -0300

    Fixes + additions

--- src/makai/lang/anima/v2/toolchain/assembler/semibreve.cpp
+++ src/makai/lang/anima/v2/toolchain/assembler/semibreve.cpp
@@ -2,7 +2,7 @@
 #include "breve.hpp"
 #include "context.hpp"
 #include "core.hpp"
-#include "makai/data/hash.hpp"
+#include "../../../../../data/data.hpp"
 
 using namespace Makai::Anima::V2::Toolchain::Assembler;
 using namespace Makai::Error;
