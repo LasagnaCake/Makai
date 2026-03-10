5a68613cf86d2b9e44a9b4f27bf8aa74285550fa
LasagnaCake <sponge.cake@tuta.io>
2026-03-10 10:36:02 -0300

    Update parser.cpp

--- src/makai/lang/anima/v2/toolchain/compiler/parser.cpp
+++ src/makai/lang/anima/v2/toolchain/compiler/parser.cpp
@@ -103,6 +103,8 @@ Parser::Parser(BaseContext& context): context(context) {
 		LTS_TT_LOGIC_NOT,
 		LTS_TT_BIT_NOT,
 		LTS_TT_RAISE,
+		LTS_TT_DOLLAR,
+		LTS_TT_SHARP,
 		LTS_TT_AMP,
 		LTS_TT_STAR
 	);
