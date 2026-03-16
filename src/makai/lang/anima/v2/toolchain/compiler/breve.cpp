39b39b48c69b83179afdde839547662cdbb93909
LasagnaCake <sponge.cake@tuta.io>
2026-03-16 17:36:39 -0300

    Update breve.cpp

---
+++ src/makai/lang/anima/v2/toolchain/compiler/breve.cpp
@@ -0,0 +1,16 @@
+#include "breve.hpp"
+
+using namespace Makai::Anima::V2::Core;
+
+using namespace Makai::Anima::V2::Toolchain::Compiler;
+
+using Context = Breve::Context;
+
+using Type = Context::Tokenizer::Token::Type;
+using enum Type;
+
+CTL_DIAGBLOCK_BEGIN
+CTL_DIAGBLOCK_IGNORE_SWITCH
+
+
+CTL_DIAGBLOCK_END
