23338d960658431a1a760a52d05d2cbd43601046
LasagnaCake <sponge.cake@tuta.io>
2026-03-26 14:49:42 -0300

    Update resolver.cpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/resolver.cpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/resolver.cpp
@@ -107,7 +107,7 @@ Node::Instance FunctionCallResolver::resolve(Parser& parser, Node::Instance cons
 			parser.context.next();
 			break;
 		}
-		result->children.pushBack(parser.nextExpression());
+		result->children.pushBack(parser.nextExpression(precedence));
 		DEBUGLN("Argument: ", result->children.back()->base.text);
 		if (parser.context.peek().type == (LTS_TT_CLOSE_PAREN)) {
 			DEBUGLN("No more arguments!");
