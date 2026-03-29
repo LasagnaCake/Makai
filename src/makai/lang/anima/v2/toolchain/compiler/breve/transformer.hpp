d783c6af2dd40889856239e8957e68295cc339d8
LasagnaCake <sponge.cake@tuta.io>
2026-03-29 20:20:04 -0300

    Update transformer.hpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/transformer.hpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/transformer.hpp
@@ -109,6 +109,10 @@ namespace Makai::Anima::V2::Toolchain::Compiler::Breve::Transformer {
 		Result transform(Context& context, Node::Instance const& node) override;
 	};
 
+	struct Direct: ATransformer {
+		Result transform(Context& context, Node::Instance const& node) override;
+	};
+
 	struct SubExpression: ATransformer {
 		Result transform(Context& context, Node::Instance const& node) override;
 	};
