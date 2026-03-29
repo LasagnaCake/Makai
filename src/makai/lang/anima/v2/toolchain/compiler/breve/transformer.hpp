562d78d08c4b22891c3dbee01cf4cdec3f2ef878
LasagnaCake <sponge.cake@tuta.io>
2026-03-28 23:32:28 -0300

    Update transformer.hpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/transformer.hpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/transformer.hpp
@@ -53,7 +53,7 @@ namespace Makai::Anima::V2::Toolchain::Compiler::Breve::Transformer {
 
 		bool allowPaths = true;
 
-		Namespace::Instance resolve(Context& context, Node::Instance const& node) const;
+		KeyValuePair<UTF8StringList, Namespace::Instance> resolve(Context& context, Node::Instance const& node) const;
 
 		virtual Result transform(Context& context, Node::Instance const& node) = 0;
 	};
