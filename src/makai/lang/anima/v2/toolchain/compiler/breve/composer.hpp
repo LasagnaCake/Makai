8e9f26020523c6ccf6fd21e5874933605b120c60
LasagnaCake <sponge.cake@tuta.io>
2026-04-14 22:33:10 -0300

    Update composer.hpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/composer.hpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/composer.hpp
@@ -14,6 +14,7 @@ namespace Makai::Anima::V2::Toolchain::Compiler::Breve {
 		UTF8StringList types;
 		UTF8StringList functions;
 		List<Implementation::Instance> funcDefs;
+		List<Implementation::Instance> staticDefs;
 
 		usize staticVarCount = 0;
 
