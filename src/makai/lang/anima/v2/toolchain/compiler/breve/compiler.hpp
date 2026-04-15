9c0d16a8f5606aa96200a075c591612b83064a86
LasagnaCake <sponge.cake@tuta.io>
2026-04-15 08:41:33 -0300

    Update compiler.hpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/compiler.hpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/compiler.hpp
@@ -8,12 +8,11 @@
 #include "transformer.hpp"
 
 namespace Makai::Anima::V2::Toolchain::Compiler::Breve {
-	struct Compiler: ACompiler, IValue<UTF8String>  {
+	struct Compiler: ACompiler {
 		struct Context: BaseContext, Transformer::ATransformer::Context {
 		};
 
 		void invoke() override;
-		UTF8String value() const override;
 
 		Compiler(Parser& parser, Context& context): ACompiler(context), context(context), parser(parser) {}
 
