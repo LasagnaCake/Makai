f98ed322e9a0c48ac5407b7ddc747eac3203084f
LasagnaCake <sponge.cake@tuta.io>
2026-04-21 19:14:03 -0300

    Update arte.cpp

--- tools/anima/arte.cpp
+++ tools/anima/arte.cpp
@@ -13,12 +13,33 @@ namespace ART {
 	static void writeLine(Makai::UTF8String const& str) {
 		std::cout << str << "\n";
 	}
+
+	template <class T>
+	static Makai::UTF8String toString(T const& val) {
+		if constexpr (Makai::Type::Equal<T, Makai::UTF8Char>)
+			return Makai::UTF8String(val);
+		else return Makai::toString(val);
+	}
 }
 
 struct ARTE: Makai::Anima::V2::Runtime::Engine {
 	ARTE() {
-		context.art.addExternalMethod("art/core/io/write", ART::write);
-		context.art.addExternalMethod("art/core/io/writeLine", ART::writeLine);
+		context.art.addExternalMethod("art/core/io/write_string", ART::write);
+		context.art.addExternalMethod("art/core/io/writeLine_string", ART::writeLine);
+		context.art.addExternalMethod("art/core/conv/toString_string", ART::toString<Makai::UTF8String>);
+		context.art.addExternalMethod("art/core/conv/toString_int8", ART::toString<int8>);
+		context.art.addExternalMethod("art/core/conv/toString_int16", ART::toString<int16>);
+		context.art.addExternalMethod("art/core/conv/toString_int32", ART::toString<int32>);
+		context.art.addExternalMethod("art/core/conv/toString_int64", ART::toString<int64>);
+		context.art.addExternalMethod("art/core/conv/toString_uint8", ART::toString<uint8>);
+		context.art.addExternalMethod("art/core/conv/toString_uint16", ART::toString<uint16>);
+		context.art.addExternalMethod("art/core/conv/toString_uint32", ART::toString<uint32>);
+		context.art.addExternalMethod("art/core/conv/toString_uint64", ART::toString<uint64>);
+		context.art.addExternalMethod("art/core/conv/toString_float32", ART::toString<float32>);
+		context.art.addExternalMethod("art/core/conv/toString_float64", ART::toString<float64>);
+		context.art.addExternalMethod("art/core/conv/toString_float128", ART::toString<float128>);
+		context.art.addExternalMethod("art/core/conv/toString_bool", ART::toString<bool>);
+		context.art.addExternalMethod("art/core/conv/toString_char", ART::toString<Makai::UTF8Char>);
 	}
 };
 
