ed206661ead73f0428b333c7efbdc9b80c8cd375
LasagnaCake <sponge.cake@tuta.io>
2026-04-22 20:19:53 -0300

    Update engine.cpp

--- src/makai/lang/anima/v2/runtime/engine.cpp
+++ src/makai/lang/anima/v2/runtime/engine.cpp
@@ -813,8 +813,15 @@ void Engine::v2StackBlit() {
 	auto const count = Makai::Cast::bit<uint64>(current);
 	if (!(blit.offset + count < src.size()))
 		return crash(outOfRangeError("Requested blit range falls outside source's size!"));
-	// TODO: The different blitting type stuff
 	if (blit.fromGlobal)
 		dst.appendBack(src.sliced(-(blit.offset+1 + count), -(blit.offset+1)));
 	else dst.appendBack(src.sliced(blit.offset, blit.offset + count));
+}
+
+void Engine::v2Create() {
+	// TODO: This
+}
+
+void Engine::v2Clear() {
+	// TODO: This
 }
