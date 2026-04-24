00deaf715e687754c5d6936f957b471bb8e138d4
LasagnaCake <sponge.cake@tuta.io>
2026-04-24 15:47:27 -0300

    Update context.cpp

--- src/makai/lang/anima/v2/core/context.cpp
+++ src/makai/lang/anima/v2/core/context.cpp
@@ -3,3 +3,12 @@
 
 using namespace Makai;
 using namespace Makai::Anima::V2::Core;
+
+Context::Library::~Library() {close();}
+
+bool Context::loadLibrary(Makai::String const& path) {
+	auto const lib = Library::open(path, *this);
+	if (!lib) return false;
+	dynlibs.pushBack(*lib);
+	return true;
+}
