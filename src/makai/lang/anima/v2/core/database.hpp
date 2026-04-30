753bc028b4e62143a03b9bb44cb36486e26a4491
LasagnaCake <sponge.cake@tuta.io>
2026-04-29 22:34:36 -0300

    Update database.hpp

--- src/makai/lang/anima/v2/core/database.hpp
+++ src/makai/lang/anima/v2/core/database.hpp
@@ -6,8 +6,10 @@
 namespace Makai::Anima::V2::Core {
 	template <Type::Derived<Entry> T>
 	struct Database {
-		using ElementType = Instance<T>;
-		using StorageType = List<ElementType>;
+		using ElementType		= Instance<T>;
+		using StorageType		= List<ElementType>;
+		using QueryType			= Handle<T>;
+		using QueryResultType	= List<QueryType>;
 
 		StorageType byName(String const& name) const {
 			StorageType out;
@@ -16,7 +18,7 @@ namespace Makai::Anima::V2::Core {
 					out.pushBack(v);
 			}
 			return out;
-		};
+		}
 
 		StorageType byNameHash(uint64 const& hash) const {
 			StorageType out;
@@ -25,13 +27,37 @@ namespace Makai::Anima::V2::Core {
 					out.pushBack(v);
 			}
 			return out;
-		};
+		}
 
 		ElementType byID(uint64 const id) const {
 			if (id < values.size())
 				return values[id];
 			return nullptr;
-		};
+		}
+
+		QueryResultType queryByName(String const& name) const {
+			QueryResultType out;
+			for (auto& v: values) {
+				if (v->name == name)
+					out.pushBack(v.asWeak());
+			}
+			return out;
+		}
+
+		QueryResultType queryByNameHash(uint64 const& hash) const {
+			QueryResultType out;
+			for (auto& v: values) {
+				if (v->hash == hash)
+					out.pushBack(v.asWeak());
+			}
+			return out;
+		}
+
+		QueryResultType queryByID(uint64 const id) const {
+			if (id < values.size())
+				return values[id].asWeak();
+			return nullptr;
+		}
 
 		void addElement(ElementType const& elem) {
 			values.pushBack(elem);
