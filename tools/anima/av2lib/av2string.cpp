0c35e7abf114e475f774ea501d9ed73a7e1a9be9
LasagnaCake <sponge.cake@tuta.io>
2026-04-30 08:49:30 -0300

    Renaming

--- tools/anima/av2lib/av2string.cpp
+++ tools/anima/av2lib/av2string.cpp
@@ -3,7 +3,7 @@
 using namespace Makai;
 using namespace Anima::V2::Core;
 
-struct StringManipLib: ILibrary {
+struct StringLib: ILibrary {
 
 	static String findFirst(String const& str, String const& expr) {
 		return Regex::findFirst(str, expr).match;
@@ -13,6 +13,10 @@ struct StringManipLib: ILibrary {
 		return Regex::findFirst(str, expr).position;
 	}
 
+	static StringList find(String const& str, String const& expr) {
+		return Regex::find(str, expr).toList<String>([] (auto const& e) {return e.match;});
+	}
+
 	void load(Context::TypeAdder const& types, Context::MethodAdder const& methods) {
 		methods.add("av2/string/replace", Regex::replace);
 		methods.add("av2/string/contains", Regex::contains);
@@ -20,7 +24,8 @@ struct StringManipLib: ILibrary {
 		methods.add("av2/string/count", Regex::count);
 		methods.add("av2/string/findFirst", findFirst);
 		methods.add("av2/string/firstIndexOf", firstIndexOf);
+		methods.add("av2/string/find", find);
 	}
 };
 
-AV2_Library(StringManipLib);
+AV2_Library(StringLib);
