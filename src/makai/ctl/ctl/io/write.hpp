810b78dcea245d0e625a69124a4ce08a461d2692
LasagnaCake <sponge.cake@tuta.io>
2026-05-07 02:11:56 -0300

    Create write.hpp

---
+++ src/makai/ctl/ctl/io/write.hpp
@@ -0,0 +1,15 @@
+#ifndef CTL_IO_WRITE_H
+#define CTL_IO_WRITE_H
+
+#include "console.hpp"
+#include "ansi.hpp"
+
+CTL_NAMESPACE_BEGIN
+
+namespace IO {
+	template <class... TArgs>	inline void write(TArgs const&... args)		{Console::print(toString(args)...);		}
+	template <class... TArgs>	inline void writeLine(TArgs const&... args)	{Console::println(toString(args)...);	}
+	template <class T>			inline T read()								{return Console::get<T>();				}
+}
+
+CTL_NAMESPACE_END
