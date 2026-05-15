4adbb0da093891e13ad902304b2d2f8c9fdee8ec
LasagnaCake <sponge.cake@tuta.io>
2026-05-14 21:08:33 -0300

    Update av2reflect.cpp

--- tools/anima/av2lib/av2reflect.cpp
+++ tools/anima/av2lib/av2reflect.cpp
@@ -5,21 +5,20 @@ using namespace Anima::V2::Core;
 
 struct ReflectLib: ILibrary {
 	static bool hasAttribute(Any const& obj, String const& attr) {
-		if (!obj.value)		return false;
-		if (!obj->origin)	return false;
-		return obj->origin->meta.contains(attr);
+		if (!obj.value)					return false;
+		if (!obj->getOriginalType())	return false;
+		return obj->getOriginalType()->meta.contains(attr);
 	}
 
 	static StringList attributes(Any const& obj) {
-		if (!obj.value)		return {};
-		if (!obj->origin)	return {};
-		return obj->origin->meta.keys();
+		if (!obj.value)					return {};
+		if (!obj->getOriginalType())	return {};
+		return obj->getOriginalType()->meta.keys();
 	}
 
 	static Any unpack(Any const& obj, TypeID const& attr) {
-		if (!obj.value)		return {};
-		if (!obj->origin)	return {};
-		return obj->origin->meta.keys();
+		if (!obj.value)					return {};
+		if (!obj->getOriginalType())	return {};
 	}
 
 	void load(Context::Adder const& context) override {
