1a3dc2934c1df46ef6f8f042812578276286fdbe
LasagnaCake <sponge.cake@tuta.io>
2026-07-11 09:26:04 -0300

    Update composer.cpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/composer.cpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/composer.cpp
@@ -36,6 +36,8 @@ static void doFunction(Composer& composer, Namespace::FunctionRef const& fn) {
 		if (ov->scope)
 			composer.funcDefs.pushBack(ov->scope->impl);
 	}
+	if (fn->sigEntry.size())
+		composer.functions.pushBack("@hook[\"" + sigCall->sigEntry + "\"] " + sigCall->name);
 }
 
 static void doVariable(Composer& composer, Namespace::VariableRef const& var) {
