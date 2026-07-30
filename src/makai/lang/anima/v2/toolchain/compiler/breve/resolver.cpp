71ea62f6345bc9eac6d22e6e786ddcdf2bceee52
LasagnaCake <sponge.cake@tuta.io>
2026-07-29 22:49:11 -0300

    Update resolver.cpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/resolver.cpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/resolver.cpp
@@ -14,12 +14,10 @@ Node::Instance EmptyResolver::resolve(Parser& parser, Node::Instance const& left
 }
 
 Node::Instance EnumResolver::resolve(Parser& parser, Node::Instance const& leftSide, BaseContext::Axiom const& token) {
-	// TODO: This
 	MAKAILIB_DEBUGLN_FULL("Resolving enumeration expression...");
 	Node::Instance result = Node::Instance::create();
 	result->base = token;
 	result->content = Node::Content::AV2_TANC_DECLARATION;
-	parser.context.error("Unimplemented!");
 	auto const decl = parser.nextExpression();
 	if (decl->base.type == LTS_TT_COLON) {
 		result->leftSide = decl->leftSide;
