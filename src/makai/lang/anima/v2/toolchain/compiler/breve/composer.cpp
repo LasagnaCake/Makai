6939c08d452d4f0a0db4c0cb44195d7834ee76ad
LasagnaCake <sponge.cake@tuta.io>
2026-08-14 07:35:28 -0300

    Update composer.cpp

--- src/makai/lang/anima/v2/toolchain/compiler/breve/composer.cpp
+++ src/makai/lang/anima/v2/toolchain/compiler/breve/composer.cpp
@@ -122,8 +122,8 @@ static void doType(Composer& composer, Namespace::TypeRef const& type) {
 			decl += " nil<" + type->base->name + ">";
 		else decl += " derived<" + type->base->name + ">";
 	}
-	if (type->scope && type->scope->meta) {
-		if (type->scope->meta.values().filter([] (auto const& e) {return !e->value.isUndefined();}).size()) {
+	if (type->scope) {
+		if (type->scope->meta.values().filter([] (auto const& e) {return !e || !e->value.isUndefined();}).size()) {
 			decl += "\n  meta [\n";
 			for (auto& [name, attrib]: type->scope->meta)
 				if (!attrib->value.isUndefined())
