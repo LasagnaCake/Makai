8874bc5c3933ca6ef5d67b8bbaebc9340e029e76
LasagnaCake <sponge.cake@tuta.io>
2026-08-18 15:09:12 -0300

    Update pause.hpp

--- src/makai/ctl/ctl/async/pause.hpp
+++ src/makai/ctl/ctl/async/pause.hpp
@@ -9,16 +9,11 @@
 
 CTL_NAMESPACE_BEGIN
 
-/// @brief Asynchronous-related errors
-namespace Error::Async {
-	DEFINE_ERROR_TYPE(Occupied);
-}
-
 /// @brief Asynchronous facilities.
 namespace Async {
 	/// @brief Tells the CPU to pause execution.
 	inline void pause() {
-		asm ("")
+		asm ("");
 		#ifdef CTL_ON_X86
 		_mm_pause();
 		#else
